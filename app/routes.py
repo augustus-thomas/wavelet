b4716f72d762a1e1c16492250a2f12a060f8dc39
Gus Thomas <augustusrthomas@gmail.com>
2024-04-03 10:43:33 -0400

    Issue0: Flask app displaying static image

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template, Flask
+import os
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
