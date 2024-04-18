fbfc5737f066c35b3ded0093cfc030d874431291
Gus Thomas <augustusrthomas@gmail.com>
2024-04-18 08:16:27 -0400

    everything but the actual script to run backend and save video

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,29 @@
+# flask app
 from app import app
-from flask import render_template, Flask
+# REST API enabling library
+from flask import render_template, Flask, jsonify, request
+# file structure?
 import os
+# async calls
+import subprocess
 
+# home page
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
+    # create the HTML from template
     return render_template("index.html")
+
+# running the simulation with script.py asynchronously
+@app.route('/start_sim', methods=['POST'])
+def run_sim():
+    # get arguments
+    args = request.json
+    # ???
+    # run sim async
+    scripts_arguments = ["python3", "sim.py"]
+    subprocess.run(['python3', 'script.py'])
+    # this is where we saved sim
+    simulation_url = '/static/sim.mp4'
+    # return link
+    return jsonify({'sim_url': simulation_url})
---
+++ sim.py
@@ -0,0 +1,14 @@
+import sys
+from backend import *
+
+def main():    
+    # Get the arguments
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    
+    # Print the arguments
+    print("Argument 1:", arg1)
+    print("Argument 2:", arg2)
+
+if __name__ == "__main__":
+    main()
